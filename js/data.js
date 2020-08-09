1b90e87e901248cfbbdf52d7b7f02a6a5f143c51
Emmo2gee <20649467+Emmo2gee@users.noreply.github.com>
2020-08-09 12:36:52 +0100

    json data

--- js/data.js
+++ js/data.js
@@ -1,6 +1,27 @@
 
-function test(){
-	console.log("for the memes");
-}
+  function createNode(element) {
+      return document.createElement(element);
+  }
 
-test();
+  function append(parent, el) {
+    return parent.appendChild(el);
+  }
+
+	const ul = document.getElementById('serverdata');
+	const url = 'https://api.battlemetrics.com/servers/2272069';
+	
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) {
+    let serverdata = data.results;
+    return serverdata.map(function(server) {
+      let li = createNode('li'),
+          span = createNode('span');
+      span.innerHTML = `${server.name} ${server.ip}`;
+      append(li, span);
+      append(ul, li);
+    })
+  })
+  .catch(function(error) {
+    console.log(error);
+  }); 
