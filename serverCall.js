efee5089e6f94cf03c2f9575c2088b8ac0c08b0f
Emmo2gee <20649467+Emmo2gee@users.noreply.github.com>
2020-12-06 12:31:41 +0000

    added new battlewrapper calls

--- serverCall.js
+++ serverCall.js
@@ -17,7 +17,7 @@ fetch('https://api.battlemetrics.com/servers/2272069?include=player')
 .then(data => serverContent(data));
 
 function serverContent (serverData) {
-    console.log(serverData)
+    //console.log(serverData)
     document.querySelector('.bmData').innerHTML = serverData.data.attributes.name + " " + serverData.data.attributes.details.map
 }
 
@@ -26,5 +26,8 @@ function bmWrapper() {
         console.log(result);
     });
 
+    Server.GetServerInfoByID(2272069).then((result2) => {
+        console.log(result2);
+    });
 }
 bmWrapper();
